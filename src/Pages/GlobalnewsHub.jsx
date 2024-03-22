77902293ca3e5bd01b81e98aacdd4e1d5a18404b
fiiwebarry <fiiwebarry@yahoo.com>
2024-03-22 23:58:49 +0100

    setting folder structure for news aggregator

--- src/Pages/GlobalnewsHub.jsx
+++ src/Pages/GlobalnewsHub.jsx
@@ -16,26 +16,32 @@ const GlobalnewsHub = () => {
 
   useEffect(() => {
     const getSelectedApi = async () => {
+      setIsLoading(true);
       try {
-        const guardianResponse = await guardianApi.get("/search?api-key=583de15d-29de-4ff8-89a3-ef7e8f767693", {
-          params: {
-            keyword: searchKeyword,
-            category: searchCategory
-          }
-        });
-        const newsApiResponse = await newsApi.get("/v2/everything?q=tesla&from=2024-03-20&sortBy=publishedAt&apiKey=2e5472ecb66d48f89d693af0420c25ea", {
-          params: {
-            keyword: searchKeyword,
-            category: searchCategory
-          }
-        });
+        const params = {
+          keyword: searchKeyword,
+          category: searchCategory,
+        };
+
+        const [{ data: _guardian }] = await Promise.all([guardianApi.get("/search?api-key=583de15d-29de-4ff8-89a3-ef7e8f767693", { params }),
+        ]);
+
+        // const newsApiResponse = await newsApi.get("/v2/everything?q=tesla&from=2024-03-20&sortBy=publishedAt&apiKey=2e5472ecb66d48f89d693af0420c25ea", {
+        //   params: {
+        //     keyword: searchKeyword,
+        //     category: searchCategory
+        //   }
+        // });
 
 
 
-        setFetchedData({ guardian: guardianResponse.data.response.results, news: newsApiResponse.response.data.articles });
-        console.log(setFetchedData);
+        setFetchedData({ guardian: _guardian.response.results });
+        console.log(_guardian);
       } catch (error) {
         console.error("Error fetching data:", error);
+      }
+      finally {
+        setIsLoading(false)
       }
     };
 
@@ -55,7 +61,7 @@ const GlobalnewsHub = () => {
         setSearchCategory={setSearchCategory}
       />
 
-      <DisplayBox data={fetchedData} isLoading={isLoading} setIsLoading={setIsLoading} />
+      <DisplayBox fetchedData={fetchedData} isLoading={isLoading} setIsLoading={setIsLoading} />
 
     </>
   )
--- src/components/DisplayBox.jsx
+++ src/components/DisplayBox.jsx
@@ -1,31 +1,36 @@
-
-
-const DisplayBox = ({ fetchedData, isLoading, setIsLoading }) => {
+const DisplayBox = ({ fetchedData, isLoading }) => {
     return (
         <div className="">
+            <div className="flex mx-auto justify-center">
+                <p className="font-bold p-2">NEW FEEDS</p>
+            </div>
+            <div className="container mx-auto w-[990px] border rounded-md h-auto shadow-md">
 
+                <div> {isLoading ? (
+                    <div><p>Newshub</p></div>
+                ) : (
+                    <div className="flex flex-wrap gap-[50px] justify-center p-3 mt-[10px]">
+                        {fetchedData && fetchedData.guardian ? (
+                            fetchedData.guardian.map((item, index) => (
+                                <div className="p-6 flex flex-wrap shadow-lg  bg-slate-100 w-[45%]" key={index}>
+                                    {/* Render each item from The Guardian */}
 
-
-            <div className=" flex justify-center  mx-auto w-[990px] border rounded-md h-screen shadow-md">
-                <div>
-                    <p className="font-bold p-2">NEW FEEDS</p>
-                </div>
-                {isLoading ? (<div><p>Newshub</p></div>) : (<div>
-
-                    {fetchedData.guardian && fetchedData.guardian.map((item, index) => (
-                        <div key={index}>
-                            {/* Render each item from The Guardian */}
-                            <p>{item.title}</p>
-                        </div>
-                    ))}
-                </div>)}
-
-
+                                    <p>{item.type}</p>
+                                    <p>{item.sectionId}</p>
+                                    <p>{item.sectionName}</p>
+                                    <p>{item.webTitle}</p>
+                                    <p>{item.webUrl}</p>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No data available</p>
+                        )}
+                    </div>
+                )} </div>
 
             </div>
-
         </div>
-    )
+    );
 }
 
-export default DisplayBox
+export default DisplayBox;
